a8ebefa322cb4abab65d76785a12e46a443bf562
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-12-03 21:19:27 +0000

    Fixed n-gram order (may still need to remove useless words, such as "'s")

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import operator
+
 from keras.datasets import imdb
+from keras_preprocessing.text import Tokenizer
 from nltk import word_tokenize
 from nltk.util import ngrams
-from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
 import nltk
+
 nltk.download('punkt')
 (training_data, training_targets), (testing_data, testing_targets) = imdb.load_data()
+
 
 # index = imdb.get_word_index()
 # reverse_index = dict([(value, key) for (key, value) in index.items()])
@@ -14,55 +19,52 @@ nltk.download('punkt')
 
 def get_popular_ngrams(sentences):
     unigrams, bigrams = extract_ngrams(sentences)
-    popular_unigrams, popular_bigrams = Counter(unigrams).most_common(20), Counter(bigrams).most_common(20)
-
     i = 0
     print("Top 20 unigrams")
-    for unigram in popular_unigrams:
+    for unigram in dict(sorted(unigrams.items(), key=lambda item: item[1], reverse=True)):
         i += 1
-        print(str(i) + ". " + unigram[0] + " : " + str(unigram[1]))
+        if i > 20:
+            break
+        print(str(i), ". ", unigram, " :: ", unigrams[unigram])
 
     i = 0
     print("\nTop 20 bigrams")
-    for bigram in popular_bigrams:
+    for bigram in dict(sorted(bigrams.items(), key=lambda item: item[1], reverse=True)):
         i += 1
-        print(str(i) + ". " + "{ " + bigram[0][0] + ", " + bigrams[0][1] + " } : " + str(bigram[1]))
+        if i > 20:
+            break
+        print(str(i), ". ", "{ ", bigram, " } : ", bigrams[bigram])
 
 
 def extract_ngrams(sentences):
-    unigrams = []
-    bigrams = []
+    unigrams = {}
+    bigrams = {}
     for sentence in sentences:
         tokens = word_tokenize(sentence)
 
-        temp = extract_unigrams(tokens)
-        if temp:
-            for u in temp:
-                unigrams.append(u)
+        for token in tokens:
+            if token not in unigrams:
+                unigrams[token] = 1
+            else:
+                unigrams[token] += 1
 
-        temp = extract_bigrams(tokens)
-        if temp:
-            for b in temp:
-                bigrams.append(b)
+        grams = ngrams(tokens, 2)
+        for gram in grams:
+            if gram not in bigrams:
+                bigrams[gram] = 1
+            else:
+                bigrams[gram] += 1
 
     return unigrams, bigrams
 
 
-def extract_unigrams(tokens):
-    unigrams = []
-    for token in tokens:
-        if token not in unigrams:
-            unigrams.append(token)
-    return unigrams
-
-
-def extract_bigrams(tokens):
-    bigrams = []
-    grams = ngrams(tokens, 2)
-    for gram in grams:
-        if gram not in bigrams:
-            bigrams.append(gram)
-    return bigrams
-
-
 get_popular_ngrams(imdb.get_word_index())
+
+# vectorizer = CountVectorizer()
+# imdb_bag_of_words = vectorizer.fit_transform(imdb.get_word_index())
+# vectorizer.get_feature_names()
+# bow_vector = imdb_bag_of_words.toarray()
+# vectorizer.vocabulary_.get('awesome')
+# vectorizer.transform(['Something completely new.']).toarray()
+
+# tokenizer = Tokenizer(imdb_bag_of_words.size())
