43f38aad0f9c9f34b5db831869ab37d594cfa7f2
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-12-04 14:10:47 +0000

    n-grams possibly fully implemented (now decodes the training/testing set to obtain the words); it could maybe be improved for decisions (read the TODOs)

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import operator
-
 from keras.datasets import imdb
 from keras_preprocessing.text import Tokenizer
 from nltk import word_tokenize
 from nltk.util import ngrams
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk
 
 nltk.download('punkt')
+nltk.download('stopwords')
+
+# splits the data into training and testing data, the target data signifies whether or not each sentence will be used
+# for training/testing TODO (why?)
 (training_data, training_targets), (testing_data, testing_targets) = imdb.load_data()
 
-
-# index = imdb.get_word_index()
-# reverse_index = dict([(value, key) for (key, value) in index.items()])
-# decoded = " ".join([reverse_index.get(i - 3, "#") for i in training_data[0]])
-# print(decoded)
+# inverts the { word: ID } dictionary order
+index = dict([(value, key) for (key, value) in imdb.get_word_index().items()])
 
 
 def get_popular_ngrams(sentences):
-    unigrams, bigrams = extract_ngrams(sentences)
+    decoded = []
+    occlude = stopwords.words('english')
+
+    # TODO are these needed for better sentiment analysis? it would appear that they are (for example, there's a
+    # TODO difference between "could" and "couldn't") but it would be better if the tokenizer would not split these
+    for o in ['br', '#', '\'', '\'ve', 'n\'t', '\'s']:
+        occlude.append(o)
+    for sentence in sentences:
+        decoded.append(" ".join([index.get(wordID - 3, "#") for wordID in sentence]))
+    unigrams, bigrams = extract_ngrams(decoded, occlude)
+
     i = 0
     print("Top 20 unigrams")
     for unigram in dict(sorted(unigrams.items(), key=lambda item: item[1], reverse=True)):
         i += 1
         if i > 20:
             break
-        print(str(i), ". ", unigram, " :: ", unigrams[unigram])
+        print(str(i) + ".", unigram, "::", unigrams[unigram])
 
     i = 0
     print("\nTop 20 bigrams")
@@ -33,32 +43,37 @@ def get_popular_ngrams(sentences):
         i += 1
         if i > 20:
             break
-        print(str(i), ". ", "{ ", bigram, " } : ", bigrams[bigram])
+        print(str(i) + ".", bigram, "::", bigrams[bigram])
 
 
-def extract_ngrams(sentences):
+def extract_ngrams(sentences, occlude):
     unigrams = {}
     bigrams = {}
     for sentence in sentences:
-        tokens = word_tokenize(sentence)
+        # we use TweetTokenizer as it doesn't split words by apostrophe - TODO is this better?
+        words = nltk.TweetTokenizer().tokenize(sentence)
 
-        for token in tokens:
-            if token not in unigrams:
-                unigrams[token] = 1
-            else:
-                unigrams[token] += 1
+        for word in words:
+            if word not in occlude:
+                if word not in unigrams:
+                    unigrams[word] = 1
+                else:
+                    unigrams[word] += 1
 
-        grams = ngrams(tokens, 2)
+        grams = ngrams(words, 2)
         for gram in grams:
-            if gram not in bigrams:
-                bigrams[gram] = 1
-            else:
-                bigrams[gram] += 1
+            if gram[0] not in occlude and gram[1] not in occlude:
+                if gram not in bigrams:
+                    bigrams[gram] = 1
+                else:
+                    bigrams[gram] += 1
 
     return unigrams, bigrams
 
 
-get_popular_ngrams(imdb.get_word_index())
+# print(decoded)
+
+get_popular_ngrams(training_data)
 
 # vectorizer = CountVectorizer()
 # imdb_bag_of_words = vectorizer.fit_transform(imdb.get_word_index())
