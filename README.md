7ce133197434db9a761dd87b5f80acf348a4e4b3
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-12-13 12:42:57 +0000

    Cleaned up code, added more comments, and added more parameters for better flexibility

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Sentiment-Analysis
 A sentiment analysis tool proposed by Edinburgh Napier University. Built to understand the whether a set of movie reviews are positive or negative.
 
-The tool must:
+According to the specification, the tool must:
 * Decode the lists of word IDs (using each word's respective ID from an index).
 * List the 20 most popular unigrams and bigrams.
 * Utilize a decision tree to predict the sentiment of reviews.
--- sentiment_classification.py
+++ sentiment_classification.py
@@ -1,4 +1,4 @@
-from sklearn import metrics
+from sklearn import metrics, logger
 from keras.datasets import imdb
 from six import StringIO
 from sklearn.feature_extraction.text import CountVectorizer
@@ -50,47 +50,55 @@ def decode_sentence(sentence):  # converts a sentence from a list of integers (w
     return decoded_sentence
 
 
-def extract_data(data, is_test=False):
+def extract_data(data, is_train=False, is_test=False):
     unigrams_ = {}
     bigrams_ = {}
     sentence_vocabularies = []
     concatenated_bigrams_str = ""
+    bags_of_words_ = []
 
-    for sentence in data:
-        decoded_sentence = decode_sentence(sentence)
+    try:
+        if is_train and is_test:
+            raise ValueError('\'is_train\' and \'is_test\' were true; we can\'t train with test data and vice-versa.')
+        for sentence in data:
+            decoded_sentence = decode_sentence(sentence)
 
-        # this allows differentiation of tokenizing as a Tweet - TweetTokenizer doesn't tokenize words by characters
-        # like an apostrophe or a hyphen, i.e. we don't get unigrams such as "it" and "'s", we now get "it's"
-        decoded_tokenized = TweetTokenizer.tokenize(TweetTokenizer(), decoded_sentence) if use_tweet_tokenizer else \
-            word_tokenize(decoded_sentence)
+            # this allows differentiation of tokenizing as a Tweet - TweetTokenizer doesn't tokenize words by characters
+            # like an apostrophe or a hyphen, i.e. we don't get unigrams such as "it" and "'s", we now get "it's"
+            decoded_tokenized = TweetTokenizer.tokenize(TweetTokenizer(), decoded_sentence) if use_tweet_tokenizer \
+                else word_tokenize(decoded_sentence)
 
-        if not is_test:  # we won't need unigrams for testing purposes
-            unigrams_ = extract_unigrams(unigrams_, decoded_tokenized)
+            if not is_test:  # we won't need unigrams for testing purposes
+                unigrams_ = extract_unigrams(unigrams_, decoded_tokenized)
 
-        # we only need the bigrams during testing is we want a vocabulary of bigrams
-        if not is_test or (is_test and use_bigrams):
-            # concatenated_bigrams - used to add the bigrams to the vocabulary
-            # concatenated_bigrams_str - used to get the bigrams in the bag of words
-            bigrams_, concatenated_bigrams_str, concatenated_bigrams = extract_ngrams(bigrams_, decoded_tokenized)
+            # during testing, we only need bigrams if we want a vocabulary of bigrams
+            if not is_test or (is_test and use_bigrams):
+                # concatenated_bigrams - used to add the bigrams to the vocabulary
+                # concatenated_bigrams_str - used to get the bigrams in the bag of words
+                bigrams_, concatenated_bigrams_str, concatenated_bigrams = extract_ngrams(bigrams_, decoded_tokenized)
 
-        if use_bigrams:  # builds sentences using either unigrams or bigrams
-            sentence_vocabularies.append(concatenated_bigrams_str)
-        else:
-            sentence_vocabularies.append(decoded_sentence[3:])
+            if use_bigrams:  # builds sentences using either unigrams or bigrams
+                sentence_vocabularies.append(concatenated_bigrams_str)
+            else:
+                sentence_vocabularies.append(decoded_sentence[3:])  # start at index 3 as 0-2 = ' # '
 
-    if not is_test:
-        vectorizer.fit(unigrams_.keys())
+        if is_train:  # initialise the vocabulary with the unique words in training data
+            vectorizer.fit(unigrams_.keys())
 
-    # vectorizer.transform creates a bag of words (as count vectors) - a list of the count of each word of the
-    # vocabulary in the sentence (0 if a word isn't in the sentence)
-    return unigrams_, bigrams_, vectorizer.transform(vocabulary for vocabulary in sentence_vocabularies).toarray()
+        # vectorizer.transform creates a bag of words (as count vectors) - a list of the count of each word of the
+        # vocabulary in the sentence (0 if a word isn't in the sentence)
+        bags_of_words_ = vectorizer.transform(vocabulary for vocabulary in sentence_vocabularies).toarray()
+    except Exception as e:
+        logger.error(repr(e.args))
+
+    return unigrams_, bigrams_, bags_of_words_
 
 
 def extract_unigrams(unigrams_, tokenized_sentence):
     for word in tokenized_sentence:
         # prevents stand-alone apostrophes from being used as unigrams
         if not (word in system_occlusions or (use_tweet_tokenizer and word[0] == '\'')):
-            if word not in unigrams_:
+            if word not in unigrams_:  # counts the occurrence of each unique unigram
                 unigrams_[word] = 1
             else:
                 unigrams_[word] += 1
@@ -109,7 +117,7 @@ def extract_ngrams(ngrams_, tokenized_sentence, n=2):
                                                                                                             "'" or gram[
                                                                                                                 1] ==
                                                                                                             "'"))):
-            if gram not in ngrams_:
+            if gram not in ngrams_:  # counts the occurrence of each unique bigram
                 ngrams_[gram] = 1
 
                 if use_bigrams:
@@ -139,10 +147,10 @@ def print_popular_ngrams(unigrams_, bigrams_):  # sorts the unigrams and bigrams
         print(str(i) + ".", bigram, "::", bigrams_[bigram])
 
 
-def analyse_data(training_bag_of_words):
+def analyse_data(training_bag_of_words, depth=5):
     # trains the tree using the training data as bags of words with their respective labels (sentiments)
-    tree = DecisionTreeClassifier(criterion='entropy', max_depth=5).fit(training_bag_of_words,
-                                                                        training_targets[:training_dim])
+    tree = DecisionTreeClassifier(criterion='entropy', max_depth=depth).fit(training_bag_of_words,
+                                                                            training_targets[:training_dim])
 
     # gets only the testing data bag of words from the extract
     data_in_range = extract_data(testing_data[:testing_dim], is_test=True)[2]
@@ -154,13 +162,17 @@ def analyse_data(training_bag_of_words):
     print('\nConfusion matrix:\n', metrics.confusion_matrix(targets_in_range, tree.predict(data_in_range)),
           '\nMatrix Column and Row labels (both are in the same order):\n 1 = False\n 2 = True')
 
-    if export_tree_model:  # outputs a GraphViz tree object as an SVG
+    export_tree(tree)
+
+
+def export_tree(tree):  # outputs a GraphViz tree object as an SVG
+    if export_tree_model:
         dot_data = StringIO()
         export_graphviz(tree, out_file=dot_data, filled=True, rounded=True, special_characters=True)
         graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
         graph.write(path='tree.svg', format='svg')
 
 
-unigrams, bigrams, bags_of_words = extract_data(training_data[:training_dim])
+unigrams, bigrams, bags_of_words = extract_data(training_data[:training_dim], is_train=True)
 print_popular_ngrams(unigrams, bigrams)
 analyse_data(bags_of_words)
